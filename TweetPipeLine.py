9bb4c0b959ad179d5df6e480415f8a811bfd289d
Swen <swenmulderij@gmail.com>
2014-03-24 09:37:16 +0100

    Pipeline Changed

--- TweetPipeLine.py
+++ TweetPipeLine.py
@@ -5,12 +5,12 @@ api = TwitterAPI('CsMWX8vAweukXhwNN30PA','FGjpr2Eq8FhPH4xSOFcmpuWe1zu64L0cPwq9cM
 # BB codes (SWLONG,SWLAT,NELONG,NELAT):
 # Nederland = '3.2,50.7,7.3,53.7'
 
-connection = sqlite3.connect('tweetsdb.db')
+connection = sqlite3.connect('nssdb.db')
 cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS tweets
              (user text, userid text, content text, coor text, time text, tweetid text)''')
 
-r = api.request('statuses/filter', {'track':'stemmen'})
+r = api.request('statuses/filter', {'track':'nss, nuclear security summit, nuclear'})
 print r.response
 x = 0
 for item in r.get_iterator():
@@ -21,3 +21,6 @@ for item in r.get_iterator():
 	print tweetTuple
 	cursor.execute('INSERT INTO tweets VALUES (?,?,?,?,?,?)', tweetTuple)
 	connection.commit()
+
+
+	
